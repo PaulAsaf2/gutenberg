8cd70d90f85047f262b508dadac85dc2da516468
ntsekouras <ntsekouras@outlook.com>
2025-11-06 17:51:44 +0200

    remove icons

--- packages/dataviews/src/dataviews-layouts/table/column-header-menu.tsx
+++ packages/dataviews/src/dataviews-layouts/table/column-header-menu.tsx
@@ -7,14 +7,7 @@ import type { ReactNode, Ref, PropsWithoutRef, RefAttributes } from 'react';
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import {
-	arrowLeft,
-	arrowRight,
-	chevronRight,
-	chevronLeft,
-	unseen,
-	funnel,
-} from '@wordpress/icons';
+import { arrowLeft, arrowRight, unseen, funnel } from '@wordpress/icons';
 import {
 	Button,
 	Icon,
@@ -213,7 +206,7 @@ const _HeaderMenu = forwardRef( function HeaderMenu< Item >(
 							<Menu.Group>
 								{ canMove && (
 									<Menu.Item
-										prefix={ <Icon icon={ chevronLeft } /> }
+										prefix={ <Icon icon={ arrowLeft } /> }
 										disabled={ index < 1 }
 										onClick={ () => {
 											onChangeView( {
@@ -241,9 +234,7 @@ const _HeaderMenu = forwardRef( function HeaderMenu< Item >(
 								) }
 								{ canMove && (
 									<Menu.Item
-										prefix={
-											<Icon icon={ chevronRight } />
-										}
+										prefix={ <Icon icon={ arrowRight } /> }
 										disabled={
 											index >= visibleFieldIds.length - 1
 										}
@@ -273,11 +264,7 @@ const _HeaderMenu = forwardRef( function HeaderMenu< Item >(
 								) }
 								{ canMove && !! hiddenFields.length && (
 									<Menu>
-										<Menu.SubmenuTriggerItem
-											prefix={
-												<Icon icon={ arrowLeft } />
-											}
-										>
+										<Menu.SubmenuTriggerItem>
 											<Menu.ItemLabel>
 												{ __( 'Insert left' ) }
 											</Menu.ItemLabel>
@@ -316,11 +303,7 @@ const _HeaderMenu = forwardRef( function HeaderMenu< Item >(
 								) }
 								{ !! hiddenFields.length && (
 									<Menu>
-										<Menu.SubmenuTriggerItem
-											prefix={
-												<Icon icon={ arrowRight } />
-											}
-										>
+										<Menu.SubmenuTriggerItem>
 											<Menu.ItemLabel>
 												{ __( 'Insert right' ) }
 											</Menu.ItemLabel>
